2a99fca7a40577b167ceee49262b70de32ff9c8a
Pawel Stankiewicz <40539746@live.napier.ac.uk>
2022-03-21 17:34:11 +0000

    + db connection timing

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -32,12 +32,12 @@ public class App {
             System.exit(-1);
         }
 
-        int retries = 10;
+        int retries = 30;
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
             try {
                 // Wait a bit for db to start
-                Thread.sleep(3000);
+                Thread.sleep(2000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
